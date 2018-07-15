c94ce4f1d5fdfdb59663d52d551ccec6cca51cbf
David Fry <david.fry.tv@gmail.com>
2018-07-15 12:50:17 -0700

    Switched firebase over to the prooduction server

--- config/firebase.js
+++ config/firebase.js
@@ -1,14 +1,28 @@
 // Initialize
 import * as firebase from 'firebase';
 
+// Dev Config
+
+// const config = {
+//     apiKey: "AIzaSyD1L7J20YhcGtz4pCi_gWbRrSRra3g6e80",
+//     authDomain: "enviroturfbackend.firebaseapp.com",
+//     databaseURL: "https://enviroturfbackend.firebaseio.com",
+//     projectId: "enviroturfbackend",
+//     storageBucket: "enviroturfbackend.appspot.com",
+//     messagingSenderId: "37648242895"
+// };
+
+// Production Config
+
 const config = {
-    apiKey: "AIzaSyD1L7J20YhcGtz4pCi_gWbRrSRra3g6e80",
-    authDomain: "enviroturfbackend.firebaseapp.com",
-    databaseURL: "https://enviroturfbackend.firebaseio.com",
-    projectId: "enviroturfbackend",
-    storageBucket: "enviroturfbackend.appspot.com",
-    messagingSenderId: "37648242895"
+    apiKey: "AIzaSyC5criSvQCU8vFgbYQRu0ZgRqzll1ljdRA",
+    authDomain: "enviroturf-production-backend.firebaseapp.com",
+    databaseURL: "https://enviroturf-production-backend.firebaseio.com",
+    projectId: "enviroturf-production-backend",
+    storageBucket: "",
+    messagingSenderId: "73588798403"
 };
+
 
 export const homepageDBId = "-LGwp2zviwPGVvEJLTAc";
 
